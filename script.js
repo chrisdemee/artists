f2ca2fea5a132c16b373e2bf2b535817668b93d5
chrisdemee <526cdemetri@frhsd.com>
2025-04-27 22:35:54 +0000

    still some bugs but its starting to come out good

--- script.js
+++ script.js
@@ -3,7 +3,33 @@ var artists = [
     "Tyler, The Creator", "Radiohead", "Kendrick Lamar", "Travis Scott",
     "Frank Ocean", "JID", "Daniel Caesar", "Jpegmafia", "Deftones", "Denzel Curry"
   ];
-  
+  //images and captions 
+  var artistImages = [
+    "images/tyler.jpg",
+    "images/billie.jpg",
+    "images/kendrick.jpg",
+    "images/lana.jpg",
+    "images/frank.jpg",
+    "images/doja.jpg",
+    "images/sza.jpg",
+    "images/gambino.jpg",
+    "images/weeknd.jpg",
+    "images/paramore.jpg"
+  ];
+
+  var artistCaptions = [
+    "Odd Future king 🎩",
+    "Bad guy energy 🌪️",
+    "Pulitzer prize poet 📝",
+    "Queen of dreamy sadness 🌊",
+    "Mysterious R&B genius 🌈",
+    "Hot Pink vibes 💖",
+    "Ctrl over the soul 🎶",
+    "This is America 🇺🇸",
+    "Starboy forever 🌟",
+    "Riot legends ⚡"
+  ];
+
   // HOME: Random artist generator
   function showArtist() {
     var randomIndex = Math.floor(Math.random() * artists.length);
@@ -17,10 +43,39 @@ var artists = [
     for (var i = 0; i < artists.length; i++) {
       var item = document.createElement("li");
       item.innerText = artists[i];
+      
+      (function(index) {
+        item.addEventListener('mouseover', function() {
+          showCard(index);
+        });
+        item.addEventListener('mouseout', function() {
+          hideCard();
+        });
+      })(i);
+      
       list.appendChild(item);
     }
   }
   
+
+// Show card on hover
+function showCard(index) {
+  var card = document.getElementById('hover-card');
+  var cardImage = document.getElementById('card-image');
+  var cardCaption = document.getElementById('card-caption');
+  
+  cardImage.src = artistImages[index];
+  cardCaption.innerText = artistCaptions[index];
+  
+  card.classList.add('active');
+}
+
+// Hide card when not hovering
+function hideCard() {
+  var card = document.getElementById('hover-card');
+  card.classList.remove('active');
+}
+
   // drag and drop
   var dragged;
   
